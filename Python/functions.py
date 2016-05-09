09f498873c4ae13be745a015fd67a7450b750816
Mike Dallas <mc-dallas@hotmail.com>
2016-05-09 18:30:28 +0100

    new functions

--- Python/functions.py
+++ Python/functions.py
@@ -8,13 +8,20 @@ def digits(n):
 
 # Primality test
 def pt(n):
-     for i in range(2,int(sqrt(n))+1):
-         if not n % i:
-             return False
-     return True
+    if n < 2: return False
+    for i in range(2,int(sqrt(n))+1):
+        if not n % i:
+            return False
+    return True
 
 def prime(n=2, end=float('inf')):
     while n <= end:
         if pt(n):
             yield n
         n += 1
+
+def ispalindrome(n):
+    l = list(str(n))
+    if l == l[::-1]:
+        return True
+    return False
